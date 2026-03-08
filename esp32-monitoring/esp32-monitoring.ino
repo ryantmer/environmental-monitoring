cdaa8c35b635547cc4b2125e7f70d34423eda1a8
Ryan Mahler <ryantmer@gmail.com>
2026-03-07 16:52:09 -0800

    Add screen timeout
    Screen power is now controlled via a 2N4401 on GPIO2, triggered by a
    button on GPIO3. When pressed, the screen will re-init and display
    updated data every second for a few seconds before going back to an off
    state.
    
    Also updated timezone to be correct. I do not live in UTC.

--- esp32-monitoring/esp32-monitoring.ino
+++ esp32-monitoring/esp32-monitoring.ino
@@ -6,11 +6,14 @@
 #include "ESPAsyncWebServer.h"
 #include "time.h"
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 32 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define PIN_DHT 1
+#define PIN_SCREEN_ENABLE 2
+#define PIN_SCREEN_BUTTON 3
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C // 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const char* ssid = "network name";
@@ -22,36 +25,33 @@ IPAddress dns(192, 168, 1, 2);
 
 const char* ntpServer = "pool.ntp.org";
 const long gmtOffsetSeconds = 0;
-const int daylightOffsetSeconds = 3600;
+const int daylightOffsetSeconds = 0;
+String timezone = "PST8PDT,M3.2.0,M11.1.0";
 struct tm timeinfo;
 
-#define DHT_PIN 1
 #define DHT_TYPE DHT11
-DHT dht(DHT_PIN, DHT_TYPE);
+DHT dht(PIN_DHT, DHT_TYPE);
 
 AsyncWebServer server(80);
-String readDHTTemperature() {
+
+String readTemperature() {
   float t = dht.readTemperature();
   if (isnan(t)) {    
     Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
-  else {
-    Serial.println(t);
-    return String(t);
-  }
+
+  return String(t);
 }
 
-String readDHTHumidity() {
+String readHumidity() {
   float h = dht.readHumidity();
   if (isnan(h)) {
     Serial.println("Failed to read from DHT sensor!");
     return "--";
   }
-  else {
-    Serial.println(h);
-    return String(h);
-  }
+
+  return String(h);
 }
 
 String getTimestamp() {
@@ -59,7 +59,6 @@ String getTimestamp() {
     char timeBuf[20];
     strftime(timeBuf, sizeof(timeBuf), "%FT%T", &timeinfo);
     String asString(timeBuf);
-    Serial.println(timeBuf);
     return timeBuf;
   } else {
     Serial.println("Failed to fetch time");
@@ -99,9 +98,9 @@ const char index_html[] PROGMEM = R"rawliteral(
 </html>)rawliteral";
 String processor(const String& var){
   if(var == "TEMPERATURE") {
-    return readDHTTemperature();
+    return readTemperature();
   } else if(var == "HUMIDITY") {
-    return readDHTHumidity();
+    return readHumidity();
   } else if(var == "TIMESTAMP") {
     return getTimestamp();
   }
@@ -121,58 +120,98 @@ void setupWiFi() {
   Serial.println("");
   Serial.println("WiFi connected.");
   Serial.println(WiFi.localIP());
+}
 
-  // WiFi.disconnect(true);
-  // WiFi.mode(WIFI_OFF);
+int lastButtonState = 0;
+bool checkScreenOn() {
+  int buttonState = digitalRead(PIN_SCREEN_BUTTON);
+  if (buttonState == lastButtonState) {
+    return false;
+  }
+
+  lastButtonState = buttonState;
+  if (buttonState == LOW) {
+    Serial.println("Screen on");
+    digitalWrite(PIN_SCREEN_ENABLE, HIGH);
+    return true;
+  } else {
+    Serial.println("Screen off");
+    digitalWrite(PIN_SCREEN_ENABLE, LOW);
+    return false;
+  }
+}
+
+void initDisplay() {
+  pinMode(PIN_SCREEN_ENABLE, OUTPUT);
+  pinMode(PIN_SCREEN_BUTTON, INPUT);
+  digitalWrite(PIN_SCREEN_ENABLE, HIGH);
+
+  delay(100);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    return;
+  }
+}
+
+void updateDisplay() {
+  display.clearDisplay();
+
+  String timestamp = getTimestamp();
+  String t = readTemperature();
+  String h = readHumidity();
+  String temperature = "Temperature: " + t + "C";
+  String humidity = "Humidity: " + h + "%";
+  Serial.println(timestamp + "; " + temperature + "; " + humidity);
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(timestamp);
+  display.println(WiFi.localIP());
+  display.println(temperature);
+  display.println(humidity);
+
+  display.display();
 }
 
 void setup() {
   Serial.begin(115200);
 
-  dht.begin();
-
-  Serial.println("Waiting for display...");
-  delay(500);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
-  }
-  Serial.println("Display found.");
-  delay(500);
-
   setupWiFi();
   
   configTime(gmtOffsetSeconds, daylightOffsetSeconds, ntpServer);
+  setenv("TZ", timezone.c_str(), 1);
+  tzset();
   Serial.print("Got time from ");
   Serial.println(ntpServer);
 
+  dht.begin();
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/html", index_html, processor);
   });
   server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+    request->send_P(200, "text/plain", readTemperature().c_str());
   });
   server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+    request->send_P(200, "text/plain", readHumidity().c_str());
   });
 
   // Start server
   server.begin();
+
+  initDisplay();
+  updateDisplay();
+  delay(3000);
 }
 
 void loop() {
-  display.clearDisplay();
-
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(getTimestamp());
-  display.println(WiFi.localIP());
-  display.println("Temperature: " + readDHTTemperature() + "C");
-  display.println("Humidity: " + readDHTHumidity() + "%");
-
-  display.display();
-
-  delay(10000);
+  if (checkScreenOn()) {
+    initDisplay();
+    for (int i = 0; i < 10; i++) {
+      updateDisplay();
+      delay(1000);
+    }
+  }
+  delay(25);
 }
