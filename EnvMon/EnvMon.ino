74987b88de1ac35f862265ac16912b158cbad8a5
Ryan Mahler <ryantmer@gmail.com>
2026-04-05 12:45:02 -0700

    Move display init

--- EnvMon/EnvMon.ino
+++ EnvMon/EnvMon.ino
@@ -1,20 +1,9 @@
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_Sensor.h>
-#include <Wire.h>
 #include "ESPAsyncWebServer.h"
 
 #include <EnvMonWiFi.h>
 #include <EnvMonTime.h>
 #include <EnvMonDHT11.h>
-
-#define PIN_SCREEN_ENABLE 2
-#define PIN_SCREEN_BUTTON 3
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 32
-#define OLED_RESET -1
-#define SCREEN_ADDRESS 0x3C // 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#include <EnvMonDisplay.h>
 
 AsyncWebServer server(80);
 
@@ -58,64 +47,15 @@ String processor(const String& var){
   return String();
 }
 
-int lastButtonState = 0;
-bool checkScreenOn() {
-  int buttonState = digitalRead(PIN_SCREEN_BUTTON);
-  if (buttonState == lastButtonState) {
-    return false;
-  }
-
-  lastButtonState = buttonState;
-  if (buttonState == LOW) {
-    Serial.println("Screen on");
-    digitalWrite(PIN_SCREEN_ENABLE, HIGH);
-    return true;
-  } else {
-    Serial.println("Screen off");
-    digitalWrite(PIN_SCREEN_ENABLE, LOW);
-    return false;
-  }
-}
-
-void initDisplay() {
-  pinMode(PIN_SCREEN_ENABLE, OUTPUT);
-  pinMode(PIN_SCREEN_BUTTON, INPUT);
-  digitalWrite(PIN_SCREEN_ENABLE, HIGH);
-
-  delay(100);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    return;
-  }
-}
-
-void updateDisplay() {
-  display.clearDisplay();
-
-  String timestamp = getTimestamp();
-  String t = readTemperature();
-  String h = readHumidity();
-  String temperature = "Temperature: " + t + "C";
-  String humidity = "Humidity: " + h + "%";
-  Serial.println(timestamp + "; " + temperature + "; " + humidity);
-
-  display.setTextSize(1);
-  display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.println(timestamp);
-  display.println(getLocalIP());
-  display.println(temperature);
-  display.println(humidity);
-
-  display.display();
-}
-
 void setup() {
   Serial.begin(115200);
 
   initWiFi();
   initTime();
   initDHT11();
+  initDisplay();
+
+  updateDisplay(getTimestamp(), readTemperature(), readHumidity());
 
   // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -130,17 +70,15 @@ void setup() {
 
   // Start server
   server.begin();
-
-  initDisplay();
-  updateDisplay();
   delay(3000);
 }
 
 void loop() {
   if (checkScreenOn()) {
+    // Need to re-init display any time it is powered back on
     initDisplay();
     for (int i = 0; i < 10; i++) {
-      updateDisplay();
+      updateDisplay(getTimestamp(), readTemperature(), readHumidity());
       delay(1000);
     }
   }
---
+++ EnvMon/EnvMonDisplay.h
@@ -0,0 +1,66 @@
+#ifndef EnvMonDisplay_h
+#define EnvMonDisplay_h
+
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+#define PIN_SCREEN_ENABLE 2
+#define PIN_SCREEN_BUTTON 3
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C // 0x3D for 128x64, 0x3C for 128x32
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void initDisplay() {
+  pinMode(PIN_SCREEN_ENABLE, OUTPUT);
+  pinMode(PIN_SCREEN_BUTTON, INPUT);
+  digitalWrite(PIN_SCREEN_ENABLE, HIGH);
+
+  delay(100);
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    return;
+  }
+}
+
+int lastButtonState = 0;
+bool checkScreenOn() {
+  int buttonState = digitalRead(PIN_SCREEN_BUTTON);
+  if (buttonState == lastButtonState) {
+    return false;
+  }
+
+  lastButtonState = buttonState;
+  if (buttonState == LOW) {
+    Serial.println("Screen on");
+    digitalWrite(PIN_SCREEN_ENABLE, HIGH);
+    return true;
+  } else {
+    Serial.println("Screen off");
+    digitalWrite(PIN_SCREEN_ENABLE, LOW);
+    return false;
+  }
+}
+
+void updateDisplay(String timestamp, String temperature, String humidity) {
+  display.clearDisplay();
+
+  String t = "Temperature: " + temperature + "C";
+  String h = "Humidity: " + humidity + "%";
+  Serial.println(timestamp + "; " + temperature + "; " + humidity);
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println(timestamp);
+  display.println(getLocalIP());
+  display.println(temperature);
+  display.println(humidity);
+
+  display.display();
+}
+
+#endif
